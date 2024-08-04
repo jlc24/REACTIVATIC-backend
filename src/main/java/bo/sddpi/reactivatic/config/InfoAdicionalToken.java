a4ceb67113d4b2c1889fe0485cef9b422acb33b1
jlc24 <lcjl312.llampa@gmail.com>
2024-08-04 19:48:51 -0400

    mejora en envio de datos al inicio de sesion

--- src/main/java/bo/sddpi/reactivatic/config/InfoAdicionalToken.java
+++ src/main/java/bo/sddpi/reactivatic/config/InfoAdicionalToken.java
@@ -10,21 +10,26 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.stereotype.Component;
 
-import bo.sddpi.reactivatic.modulos.aods.IPersonasAod;
-import bo.sddpi.reactivatic.modulos.entidades.Personas;
+//import bo.sddpi.reactivatic.modulos.aods.IPersonasAod;
+import bo.sddpi.reactivatic.modulos.aods.IUsuariosAod;
+//import bo.sddpi.reactivatic.modulos.entidades.Personas;
+import bo.sddpi.reactivatic.modulos.entidades.Usuarios;
 
 @Component
 public class InfoAdicionalToken implements TokenEnhancer{
 
 	@Autowired
-	IPersonasAod iPersonasAod;
+	//IPersonasAod iPersonasAod;
+	IUsuariosAod iUsuariosAod;
 
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 		Map<String, Object> info = new HashMap<String, Object>();
-		Personas persona = iPersonasAod.infoadicional(Long.parseLong(authentication.getName()));
-		info.put("nombre", persona.getPrimerapellido()+ " "+ persona.getPrimernombre());
-		info.put("nombrecliente", persona.getPrimernombre());
+		//Personas persona = iPersonasAod.infoadicional(Long.parseLong(authentication.getName()));
+		Usuarios usuario = iUsuariosAod.dato(Long.parseLong(authentication.getName()));
+		info.put("nombre", usuario.getPersona().getPrimerapellido()+ " "+ usuario.getPersona().getPrimernombre());
+		info.put("nombrecliente", usuario.getPersona().getPrimernombre());
+		info.put("rol", usuario.getRol().getNombrerol());
 		((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(info);
 		return accessToken;
 	}
